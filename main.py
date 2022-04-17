f67f0bc3b13b04de0c94be054ec7d736a3120e9e
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2022-04-16 17:14:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import flask, os
 
 app = flask.Flask(__name__)
+global status
+status = True
 
 @app.route('/start')
 def start():
+    global status
+    status = True
     return 'started'
 
 @app.route('/stop')
 def stop():
+    global status
+    status = False
     return 'stopped'
+
+@app.route('/status')
+def status():
+    global status
+    status = False
+    if status: return 'on'
+    else: return 'off'
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
