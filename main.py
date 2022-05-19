e125dfa853d814923641282f96e35f95e79bc980
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2022-05-18 17:51:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,35 @@ global status, data
 status = True
 data = ''
 
-@app.route('/input/<passw>/<data>')
+@app.route('/input/<passw>/<data>', methods = ['POST','GET'])
 def input(passw, data):
     print(status)
     if passw == '3645':
         with open('data.txt', 'w+') as f: f.write(data)
         return 'dwrite'
 
-@app.route('/data')
+@app.route('/data', methods = ['POST','GET'])
 def data():
     with open("data.txt", "r") as f:
         return str(f.read() + ' ' + str(status))
 
-@app.route('/')
+@app.route('/', methods = ['POST','GET'])
 def main():
     return str(app.url_map)
 
-@app.route('/start')
+@app.route('/start', methods = ['POST','GET'])
 def start():
     global status
     status = True
     return 'started'
 
-@app.route('/stop')
+@app.route('/stop', methods = ['POST','GET'])
 def stop():
     global status
     status = False
     return 'stopped'
 
-@app.route('/status')
+@app.route('/status', methods = ['POST','GET'])
 def status():
     global status
     if status: return 'on'
