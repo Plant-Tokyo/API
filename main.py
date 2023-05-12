70335f7c1ac3eda47ebabba670bc69ef0bca0d0e
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-05-12 14:42:38 -0700

    Update main.py
    removed unnecessary endpoints to increase efficiency

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import flask, os
 
 app = flask.Flask(__name__)
-global status, data
-status = True
+global data
 data = ''
 
 @app.route('/input/<passw>/<data>', methods = ['POST','GET'])
-def input(passw, data):
-    print(status)
+def input(passw, data):r
     if passw == '3645':
         with open('data.txt', 'w+') as f: f.write(data)
         return 'ddwrite'
@@ -20,25 +18,5 @@ def data():
 @app.route('/', methods = ['POST','GET'])
 def main():
     return str(app.url_map)
-
-@app.route('/start', methods = ['POST','GET'])
-def start():
-    global status
-    status = True
-    return 'started'
-
-@app.route('/stop', methods = ['POST','GET'])
-def stop():
-    global status
-    status = False
-    return 'stopped'
-
-@app.route('/status', methods = ['POST','GET'])
-def status():
-    global status
-    if status: return 'on'
-    else: return 'off'
-
-status = True
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
